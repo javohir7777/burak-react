d7635d54fa6990b625d0d3442ec6281a3f92515b
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-12 12:08:22 +0500

    first commit

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react-scripts" />
+
+declare module "*.css";
+declare module "*.scss";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.jpeg";
