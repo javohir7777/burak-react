1f860e66a53e59e677de5ba8101feba31811b377
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-14 01:30:31 +0500

    feat: create routing system

---
+++ src/app/screens/About.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export const About = () => {
+  return <Container>About</Container>;
+};
---
+++ src/app/screens/Users.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export const Users = () => {
+  return <Container>Users</Container>;
+};
