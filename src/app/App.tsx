be535bdc7bc8a2424bee7dd7b3708c54a0c54d52
javohir7777 <javohirjumayev7777@gmail.com>
2026-09-21 06:01:38 +0500

    feat: publish helpPage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,10 +6,10 @@ import UserPage from "./screens/userPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
-import HelpPage from "./screens/userPage";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import HelpPage from "./screens/helpPage";
 function App() {
   const location = useLocation();
   return (
